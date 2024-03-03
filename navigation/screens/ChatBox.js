c9f736e9c19c94b1948d4396f4ab1ca5e279119c
Rizki Helmi <77191093+rizkihelmi10@users.noreply.github.com>
2024-03-03 14:49:57 +0800

    adjusting home screen and add schedule

--- navigation/screens/ChatBox.js
+++ navigation/screens/ChatBox.js
@@ -3,10 +3,16 @@ import { Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function ChatBox() {
-return (
-    <View style={{flex: 1}}>
+    const injectJavaScript = `
+    document.body.style.backgroundColor = '#000'; // Set background color to black
+    document.body.style.color = '#fff'; // Set text color to white
+  `;
+
+  return (
+    <View style={{ flex: 1 }}>
       <WebView
-        source={{uri:'http://radioppiduniachat.chatango.com/'}}
+        source={{ uri: 'http://radioppiduniachat.chatango.com/' }}
+        injectedJavaScript={injectJavaScript} // Inject JavaScript to set background color
       />
     </View>
   );
